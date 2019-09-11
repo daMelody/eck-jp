70d7c1cb6f661976d01a35d5293e3f67f9b52cd1
Daniel Melody <dmwalter12@gmail.com>
2019-09-11 19:31:01 -0400

    changed BLACK outline to WHITE

--- chapter6/one/ComplexStamper.java
+++ chapter6/one/ComplexStamper.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class ComplexStamper extends JPanel implements MouseListener {
+public class ComplexStamper extends JPanel implements MouseListener, MouseMotionListener {
 
 	public static void main(String[] args) {
 		JFrame window = new JFrame("Complex Stamper");
@@ -35,19 +35,24 @@ public class ComplexStamper extends JPanel implements MouseListener {
 		if (SwingUtilities.isRightMouseButton(evt)) {
 			g.setColor(Color.BLUE);
 			g.fillOval(x-30,y-15,60,30);
-			g.setColor(Color.BLACK);
+			g.setColor(Color.WHITE);
 			g.drawOval(x-30,y-15,60,30);
 		} else {
 			g.setColor(Color.RED);
 			g.fillRect(x-30,y-15,60,30);
-			g.setColor(Color.BLACK);
+			g.setColor(Color.WHITE);
 			g.drawRect(x-30,y-15,60,30);
 		}
 		g.dispose();
 	}
 
+
 	public void mouseEntered(MouseEvent evt) {}
 	public void mouseExited(MouseEvent evt) {}
 	public void mouseClicked(MouseEvent evt) {}
 	public void mouseReleased(MouseEvent evt) {}
+
+	// methods from MouseMotionListener
+	public void mouseMoved(MouseEvent evt) {}
+	public void mouseDragged(MouseEvent evt) {}
 }
