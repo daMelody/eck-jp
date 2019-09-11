bf723127f0d4c27a3f14c37ac25d1e915d5dcf6e
Daniel Melody <dmwalter12@gmail.com>
2019-09-11 16:47:35 -0400

    ComplexStamper is SimpleStamper rn
    code template for exercise is in.
    also added to .gitignore

---
+++ chapter6/one/ComplexStamper.java
@@ -0,0 +1,53 @@
+package chapter6.one;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ComplexStamper extends JPanel implements MouseListener {
+
+	public static void main(String[] args) {
+		JFrame window = new JFrame("Complex Stamper");
+		ComplexStamper content = new ComplexStamper();
+		window.setContentPane(content);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setLocation(120,70);
+		window.setSize(400,300);
+		window.setVisible(true);
+	}
+
+	// -------------------------------------------------
+	
+	public ComplexStamper() {
+		setBackground(Color.BLACK);
+		addMouseListener(this);
+	}
+
+
+	public void mousePressed(MouseEvent evt) {
+		if (evt.isShiftDown()) {
+			repaint();
+			return;
+		}
+		int x = evt.getX(); // where mouse is clicked
+		int y = evt.getY(); // where mouse is clicked
+		Graphics g = getGraphics();
+		if (SwingUtilities.isRightMouseButton(evt)) {
+			g.setColor(Color.BLUE);
+			g.fillOval(x-30,y-15,60,30);
+			g.setColor(Color.BLACK);
+			g.drawOval(x-30,y-15,60,30);
+		} else {
+			g.setColor(Color.RED);
+			g.fillRect(x-30,y-15,60,30);
+			g.setColor(Color.BLACK);
+			g.drawRect(x-30,y-15,60,30);
+		}
+		g.dispose();
+	}
+
+	public void mouseEntered(MouseEvent evt) {}
+	public void mouseExited(MouseEvent evt) {}
+	public void mouseClicked(MouseEvent evt) {}
+	public void mouseReleased(MouseEvent evt) {}
+}
